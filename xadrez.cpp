48bdeff409e5aa74abc5fb11af04fb8417c8ee22
OTAVIOREIS2025 <otaviogdr@hotmail.com>
2025-09-09 01:46:14 +0000

    Constantes do desafio

---
+++ xadrez.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+int main() {
+    // ============================
+    // CONSTANTES DO DESAFIO
+    // ============================
+    const int BISPO_PASSOS = 5;   // Bispo se move 5 casas
+    const int TORRE_PASSOS = 5;   // Torre se move 5 casas
+    const int RAINHA_PASSOS = 8;  // Rainha se move 8 casas
