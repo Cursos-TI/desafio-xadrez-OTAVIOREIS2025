3e4b100c757ddb0326530c2d7c19d430bba1618d
OTAVIOREIS2025 <otaviogdr@hotmail.com>
2025-09-09 01:51:58 +0000

    Inicializa Tabuleiro 8x8

--- xadrez.cpp
+++ xadrez.cpp
@@ -7,3 +7,15 @@ int main() {
     const int BISPO_PASSOS = 5;   // Bispo se move 5 casas
     const int TORRE_PASSOS = 5;   // Torre se move 5 casas
     const int RAINHA_PASSOS = 8;  // Rainha se move 8 casas
+
+    const int N = 8; // Tamanho do tabuleiro 8x8
+    char tabuleiro[N][N];
+
+    // ============================
+    // INICIALIZA TABULEIRO
+    // ============================
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            tabuleiro[i][j] = '.'; // vazio
+        }
+    }
